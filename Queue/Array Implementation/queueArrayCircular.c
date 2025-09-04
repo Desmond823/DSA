124f4943e21b08df5a112d96d0938d8066c55f84
Desmond823 <jb.machacon@gmail.com>
2025-09-04 16:15:55 +0800

    fix: redid logic

--- Queue/Array Implementation/queueArrayCircular.c
+++ Queue/Array Implementation/queueArrayCircular.c
@@ -17,6 +17,7 @@ char Front(Queue Q);
 void initQueue(Queue* Q);
 bool isEmpty(Queue Q);
 bool isFull(Queue Q);
+void printStack(Queue* Q);
 
 int main(){
     Queue Q;
@@ -27,45 +28,33 @@ int main(){
     Enqueue(&Q, 'b');
     Enqueue(&Q, 'c');
     Enqueue(&Q, 'd');
-    Enqueue(&Q, 'd');
-    Enqueue(&Q, 'd');
-    Enqueue(&Q, 'd');
-    Enqueue(&Q, 'd');
-    printf("\n%d", Q.Rear);
+    Enqueue(&Q, 'e');
+    Enqueue(&Q, 'f');
+    Enqueue(&Q, 'g');
+    Enqueue(&Q, 'g');
+    (isFull(Q) == true) ? printf("Queue is full!!\n") : printf("Queue is not full!\n");
+    Dequeue(&Q);
+
+    printStack(&Q);
+    (isEmpty(Q) == true) ? printf("\nQueue is empty!\n") : printf("\nQueue is not empty!\n");
 
 }
 
 void Enqueue(Queue* Q, char x){
-    if(isEmpty(*Q) == true){ // change this code later
-        Q->Rear++;
-        if(Q->Rear == MAX){
-            Q->Rear = 0;
-        }
+    if(isFull(*Q) != true){ 
+        Q->Rear = (Q->Rear+1) % MAX;
         Q->Elem[Q->Rear] = x;
-    }
-    else if(isFull(*Q) != true){
-        Q->Rear++;
-        if(Q->Rear == MAX){
-            Q->Rear = 0;
-        }
-        Q->Elem[Q->Rear] = x;
-    }
-    else{
-        printf("\nQueue is full!\n");
     }
 }
 
 void Dequeue(Queue* Q){
     if(isEmpty(*Q) != true){
-        Q->Front++;
-        if(Q->Front == MAX){
-            Q->Front = 0;
-        }
+        Q->Front = (Q->Front + 1) % MAX;
     }
 }
 
 char Front(Queue Q){
-    return(isEmpty(Q) != true) ? Q.Elem[Q.Front]: printf("\nList is empty!\n");
+    return(isEmpty(Q) != true) ? Q.Elem[Q.Front]: '\0';
 }
 
 void initQueue(Queue* Q){
@@ -78,4 +67,10 @@ bool isEmpty(Queue Q){
 }
 bool isFull(Queue Q){
     return (Q.Front == (Q.Rear+2) % MAX) ? true : false;
+}
+
+void printStack(Queue* Q){
+    for(; isEmpty(*Q) != true; Dequeue(Q)){
+        printf("%c ", Front(*Q));
+    }
 }
